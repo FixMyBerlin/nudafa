cffd27b3d69dfda90095c3e688a9d5bee13d6dae
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2024-03-27 15:06:49 +0100

    add max width in markdown for everything except images

--- src/proseClasses.ts
+++ src/proseClasses.ts
@@ -9,5 +9,6 @@ export const markdownProseClasses = clsx(
   "prose-h3:text-sm md:prose-h3:text-base prose-h3:font-bold", // h3 size
   "prose-h3:mt-1 prose-h1:mb-2 prose-h2:mb-2 prose-h2:mt-1 prose-h3:mb-2 prose-p:mb-2 prose-p:mt-1",
   "prose-a:underline hover:prose-a:decoration-yellow-500 hover:prose-a:text-yellow-500 active:prose-a:decoration-yellow-500 prose-a:underline-offset-2 active:prose-a:decoration-2",
-  "prose-li:m-0 prose-li:p-0"
+  "prose-li:m-0 prose-li:p-0",
+  "prose-headings:max-w-3xl prose-p:max-w-3xl prose-a:max-w-3xl prose-blockquote:max-w-3xl prose-code:max-w-3xl prose-ol:max-w-3xl prose-ul:max-w-3xl prose-table:max-w-3xl prose-hr:max-w-3xl "
 );
