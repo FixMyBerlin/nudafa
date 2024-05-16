f2efc6e5f1263610bae97b88f1a00f4e8fce3f16
Tobias <tobias@fixmycity.de>
2024-05-16 17:31:25 +0200

    Add Prettier for Astro and TW, Import sorting

---
+++ .prettierrc.mjs
@@ -0,0 +1,21 @@
+/** @type {import("prettier").Config} */
+export default {
+  semi: false,
+  singleQuote: true,
+  arrowParens: 'always',
+  printWidth: 100,
+  plugins: [
+    'prettier-plugin-astro',
+    'prettier-plugin-organize-imports',
+    'prettier-plugin-tailwindcss',
+  ],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+  tailwindFunctions: ['clsx'],
+}
