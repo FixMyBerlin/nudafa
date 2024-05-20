62688be2af50a8579045cb50e8561afdeb4bebda
Tobias <tobias@fixmycity.de>
2024-05-20 20:57:04 +0200

    Keystatic: Bicycle network improved and first content

--- keystatic/keystatic.bicyclenetworkPages.config.tsx
+++ keystatic/keystatic.bicyclenetworkPages.config.tsx
@@ -1,25 +1,22 @@
 import { collection, fields } from '@keystatic/core'
 import { keystaticTextLinkArrowConfig } from './utils/keystatic.TextLinkArrow.config'
 
-export const keystaticBicyclenetworkConfig = collection({
-  label: 'Radnetz',
+export const keystaticBicyclenetworkPagesConfig = collection({
+  label: 'Radnetz Seiten',
   slugField: 'title',
-  path: 'src/content/bicyclenetwork/*',
+  path: 'src/content/bicyclenetworkpages/*',
   format: { contentField: 'content' },
   schema: {
     menu: fields.slug({
       name: { label: 'Menüpunkt', validation: { length: { min: 1, max: 25 } } },
     }),
-    title: fields.slug({
-      name: { label: 'Title', validation: { length: { min: 1, max: 80 } } },
+    title: fields.text({
+      label: 'Title',
+      validation: { length: { min: 1, max: 80 } },
     }),
     content: fields.mdx({
       label: 'Content',
       components: { ...keystaticTextLinkArrowConfig },
-    }),
-    topics: fields.multiRelationship({
-      label: 'Themen',
-      collection: 'bicyclenetworktopics',
     }),
   },
 })
