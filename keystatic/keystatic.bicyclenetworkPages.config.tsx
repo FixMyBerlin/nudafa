7a42203c6d6e0679c876d5e5c08ec074887d9bd8
Tobias <tobias@fixmycity.de>
2024-05-20 21:15:54 +0200

    Keystatic: Improve bicycle network

--- keystatic/keystatic.bicyclenetworkPages.config.tsx
+++ keystatic/keystatic.bicyclenetworkPages.config.tsx
@@ -3,12 +3,17 @@ import { keystaticTextLinkArrowConfig } from './utils/keystatic.TextLinkArrow.co
 
 export const keystaticBicyclenetworkPagesConfig = collection({
   label: 'Radnetz Seiten',
-  slugField: 'title',
+  slugField: 'menu',
   path: 'src/content/bicyclenetworkpages/*',
   format: { contentField: 'content' },
+  columns: ['order', 'title'],
   schema: {
     menu: fields.slug({
       name: { label: 'Menüpunkt', validation: { length: { min: 1, max: 25 } } },
+    }),
+    order: fields.number({
+      label: 'Reihenfolge im Menü',
+      validation: { isRequired: true, min: 0 },
     }),
     title: fields.text({
       label: 'Title',
