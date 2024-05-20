e4ba4e252e1e0f72170bbfd0a798901d02e5259d
Tobias <tobias@fixmycity.de>
2024-05-20 14:48:58 +0200

    Keystatic: Fix types by better file separation
    The main config needs to keep the key so the `ui` part still works.
    
    Revert "Keystatic: Fix types by better file separation"
    
    This reverts commit d655976931d0fd3c7f92b942582eb29606e97478.
    
    Reapply "Keystatic: Fix types by better file separation"
    
    This reverts commit b8f2384844840303b14636465098e13adb1ce1fc.

--- keystatic/keystatic.imprint.config.tsx
+++ keystatic/keystatic.imprint.config.tsx
@@ -1,23 +1,21 @@
-import { config, fields, singleton } from '@keystatic/core'
+import { fields, singleton } from '@keystatic/core'
 
-export const keystaticImprintConfig: ReturnType<typeof config>['singletons'] = {
-  imprintpage: singleton({
-    entryLayout: 'content',
-    label: 'Impressum',
-    format: { contentField: 'content' },
-    path: 'src/content/imprintpage/',
-    schema: {
-      title: fields.text({
-        label: 'Überschrift',
-        validation: { length: { min: 1, max: 80 } },
-      }),
-      subTitle: fields.text({
-        label: 'Unterüberschrift',
-        validation: { length: { max: 160 } },
-      }),
-      content: fields.mdx({
-        label: 'Content',
-      }),
-    },
-  }),
-}
+export const keystaticImprintPageConfig = singleton({
+  entryLayout: 'content',
+  label: 'Impressum',
+  format: { contentField: 'content' },
+  path: 'src/content/imprintpage/',
+  schema: {
+    title: fields.text({
+      label: 'Überschrift',
+      validation: { length: { min: 1, max: 80 } },
+    }),
+    subTitle: fields.text({
+      label: 'Unterüberschrift',
+      validation: { length: { max: 160 } },
+    }),
+    content: fields.mdx({
+      label: 'Content',
+    }),
+  },
+})
--- keystatic/keystatic.privacy.config.tsx
+++ keystatic/keystatic.privacy.config.tsx
@@ -1,23 +1,21 @@
-import { config, fields, singleton } from '@keystatic/core'
+import { fields, singleton } from '@keystatic/core'
 
-export const keystaticPrivacyConfig: ReturnType<typeof config>['singletons'] = {
-  privacypage: singleton({
-    entryLayout: 'content',
-    label: 'Datenschutz',
-    format: { contentField: 'content' },
-    path: 'src/content/privacypage/',
-    schema: {
-      title: fields.text({
-        label: 'Überschrift',
-        validation: { length: { min: 1, max: 80 } },
-      }),
-      subTitle: fields.text({
-        label: 'Unterüberschrift',
-        validation: { length: { max: 160 } },
-      }),
-      content: fields.mdx({
-        label: 'Content',
-      }),
-    },
-  }),
-}
+export const keystaticPrivacyPageConfig = singleton({
+  entryLayout: 'content',
+  label: 'Datenschutz',
+  format: { contentField: 'content' },
+  path: 'src/content/privacypage/',
+  schema: {
+    title: fields.text({
+      label: 'Überschrift',
+      validation: { length: { min: 1, max: 80 } },
+    }),
+    subTitle: fields.text({
+      label: 'Unterüberschrift',
+      validation: { length: { max: 160 } },
+    }),
+    content: fields.mdx({
+      label: 'Content',
+    }),
+  },
+})
