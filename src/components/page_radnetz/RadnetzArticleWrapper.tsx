3235e4c2edd6724f9dd2ad5593a31ed974c1e018
Tobias <tobias@fixmycity.de>
2024-05-28 22:12:46 +0200

    Radnetz: Make content show up on desktop
    In the end the transition was the issue that hid the content on desktop. No idea why.
    
    The refactoring of components did not help with fixing the issue, but its a bit cleaner this way…

---
+++ src/components/page_radnetz/RadnetzArticleWrapper.tsx
@@ -0,0 +1,55 @@
+import { useStore } from '@nanostores/react'
+import { useEffect, useState } from 'react'
+import { RadnetzArticleWrapperDesktop } from './RadnetzArticleWrapperDesktop'
+import { RadnetzArticleWrapperMobile } from './RadnetzArticleWrapperMobile'
+import { $router } from './utils/store'
+
+type Props = {
+  articleSlug: string
+
+  title: string
+  children: React.ReactNode
+  links:
+    | {
+        display: string
+        url: string
+      }[]
+    | undefined
+}
+
+/**
+ * @desc In SSR we render all articles of the collection on every page but hide all but the current.
+ * In React we update the hidden state for the current article.
+ */
+export const RadnetzArticleWrapper = ({ articleSlug, title, children, links }: Props) => {
+  // SSR: We need to tell the router which page is pre-rendered so there is no hydration mismatch
+  if (import.meta.env.SSR) {
+    $router.open(`/radnetz/${articleSlug}`)
+  }
+  // SSR: We have to use this weird useEffect roundtrip in order to work around hydration mismatches
+  const router = useStore($router)
+  const section = router?.params.section
+  const [visible, setVisible] = useState(false)
+  useEffect(() => setVisible(section === articleSlug), [section])
+
+  return (
+    <>
+      <RadnetzArticleWrapperDesktop
+        visible={visible}
+        articleSlug={articleSlug}
+        title={title}
+        links={links}
+      >
+        {children}
+      </RadnetzArticleWrapperDesktop>
+      <RadnetzArticleWrapperMobile
+        visible={visible}
+        articleSlug={articleSlug}
+        title={title}
+        links={links}
+      >
+        {children}
+      </RadnetzArticleWrapperMobile>
+    </>
+  )
+}
--- src/components/page_radnetz/RadnetzArticleWrapperMobile.tsx
+++ src/components/page_radnetz/RadnetzArticleWrapperMobile.tsx
@@ -1,12 +1,9 @@
-import { useStore } from '@nanostores/react'
-import { useEffect, useState } from 'react'
 import { RadnetzInfo } from './RadnetzInfo'
 import { RadnetzLegend } from './RadnetzLegend'
-import { $router } from './utils/store'
 
 type Props = {
+  visible: boolean
   articleSlug: string
-
   title: string
   children: React.ReactNode
   links:
@@ -21,17 +18,13 @@ type Props = {
  * @desc In SSR we render all articles of the collection on every page but hide all but the current.
  * In React we update the hidden state for the current article.
  */
-export const RadnetzArticleWrapperMobile = ({ articleSlug, title, children, links }: Props) => {
-  // SSR: We need to tell the router which page is pre-rendered so there is no hydration mismatch
-  if (import.meta.env.SSR) {
-    $router.open(`/radnetz/${articleSlug}`)
-  }
-  // SSR: We have to use this weird useEffect roundtrip in order to work around hydration mismatches
-  const router = useStore($router)
-  const section = router?.params.section
-  const [visible, setVisible] = useState(false)
-  useEffect(() => setVisible(section === articleSlug), [section])
-
+export const RadnetzArticleWrapperMobile = ({
+  visible,
+  articleSlug,
+  title,
+  children,
+  links,
+}: Props) => {
   return (
     <article className={visible ? 'bg-green-50 md:hidden' : 'hidden'} aria-hidden={visible}>
       <h3 className="px-4 pt-4 text-lg font-bold">{title}</h3>
