a32256df9fde352fa2c567788169c2e2ef816955
Tobias <tobias@fixmycity.de>
2024-05-29 15:30:03 +0200

    MapDebugHelper: List missing images
    When styles use images that are not present in the style or added to the map, this will now how in the debug info

--- src/components/page_radnetz/MapDebugHelper/MapDebugHelper.tsx
+++ src/components/page_radnetz/MapDebugHelper/MapDebugHelper.tsx
@@ -1,4 +1,5 @@
-import type { SourceSpecification } from 'maplibre-gl'
+import type { MapStyleImageMissingEvent, SourceSpecification } from 'maplibre-gl'
+import { useState } from 'react'
 import { useMap, type AnyLayer } from 'react-map-gl/maplibre'
 import { showDebugMap } from './showDebugMap'
 
@@ -25,7 +26,10 @@ export const MapDebugHelper = () => {
   try {
     const layers = mainMap.getStyle().layers
     cleanLayers = layers.filter(
-      (layer) => 'source' in layer && !layer.source.startsWith('maptiler'),
+      (layer) =>
+        'source' in layer &&
+        !layer.source.startsWith('maptiler') &&
+        !layer.source.startsWith('openmaptiles'),
     )
 
     const sources = mainMap.getStyle().sources
@@ -40,6 +44,14 @@ export const MapDebugHelper = () => {
 
   // console.log('MapDebugHelper', { layers, sources })
 
+  // Warn when a sprite image is missing
+  const [missingImages, setMissingImages] = useState<string[]>([])
+  mainMap.getMap().on('styleimagemissing', (e: MapStyleImageMissingEvent) => {
+    const imageId = e.id
+    if (imageId === 'null') return // Conditional images with Fallback images "Fill pattern: none" result in e.id=NULL
+    setMissingImages((prev) => [...new Set([...prev, imageId])])
+  })
+
   return (
     <section className="border-xl absolute left-20 top-2.5 z-50 max-h-[98%] overflow-y-auto rounded bg-pink-500 p-1 text-xs text-white shadow-2xl print:hidden">
       <details>
@@ -53,6 +65,18 @@ export const MapDebugHelper = () => {
         <pre>{JSON.stringify(cleanLayers, undefined, 2)}</pre>
       </details>
       <hr className="border-1 my-0.5 border-gray-600" />
+
+      {missingImages.length > 0 && (
+        <>
+          <details>
+            <summary className="cursor-pointer">
+              ERROR: {missingImages.length} missing images
+            </summary>
+            <pre>{JSON.stringify(missingImages, undefined, 2)}</pre>
+          </details>
+          <hr className="border-1 my-0.5 border-gray-600" />
+        </>
+      )}
 
       <div>Zoom: {zoom}</div>
     </section>
