876f8251c8243d713c9d8c5d9d414f0d382135b2
Tobias <tobias@fixmycity.de>
2024-05-29 10:39:56 +0200

    RadnetzMap: Improve debug helper

--- src/components/page_radnetz/MapDebugHelper/MapDebugHelper.tsx
+++ src/components/page_radnetz/MapDebugHelper/MapDebugHelper.tsx
@@ -20,43 +20,46 @@ export const MapDebugHelper = () => {
   if (!showMapDebugHelper || !mainMap) return null
 
   // mainMap is not available on first render, so we need to catch the erros
-  let layers: AnyLayer[] = []
-  let sources: Record<string, SourceSpecification> = {}
+  let cleanLayers: AnyLayer[] = []
+  let cleanSources: [string, SourceSpecification][] = []
   try {
-    layers = mainMap.getStyle().layers
-    sources = mainMap.getStyle().sources
+    const layers = mainMap.getStyle().layers
+    cleanLayers = layers.filter(
+      (layer) => 'source' in layer && !layer.source.startsWith('maptiler'),
+    )
+
+    const sources = mainMap.getStyle().sources
+    cleanSources = Object.entries(sources).filter(([key, _]) => !key.startsWith('maptiler'))
   } catch (error) {
     // console.info('MapDebugHelper', error)
   }
+
   const zoom = mainMap.getZoom()
 
   mainMap.getMap().showTileBoundaries = true
 
-  console.log('MapDebugHelper', { layers, sources })
+  // console.log('MapDebugHelper', { layers, sources })
 
   return (
     <section className="border-xl absolute left-20 top-2.5 z-50 max-h-[98%] overflow-y-auto rounded bg-pink-500 p-1 text-xs text-white shadow-2xl print:hidden">
+      <button
+        onClick={() => {
+          history.replaceState(null, '', window.location.href + '#debug')
+          location.reload()
+        }}
+        className="underline hover:decoration-2"
+      >
+        Refresh
+      </button>
       <details>
-        <summary className="cursor-pointer">Sources</summary>
-        <pre>
-          {JSON.stringify(
-            Object.entries(sources).filter(([key, _]) => !key.startsWith('maptiler')),
-            undefined,
-            2,
-          )}
-        </pre>
+        <summary className="cursor-pointer">Sources ({cleanSources.length})</summary>
+        <pre>{JSON.stringify(cleanSources, undefined, 2)}</pre>
       </details>
       <hr className="border-1 my-0.5 border-gray-600" />
 
       <details>
-        <summary className="cursor-pointer">Layers</summary>
-        <pre>
-          {JSON.stringify(
-            layers.filter((layer) => 'source' in layer && !layer.source.startsWith('maptiler')),
-            undefined,
-            2,
-          )}
-        </pre>
+        <summary className="cursor-pointer">Layers ({cleanLayers.length})</summary>
+        <pre>{JSON.stringify(cleanLayers, undefined, 2)}</pre>
       </details>
       <hr className="border-1 my-0.5 border-gray-600" />
 
