18b71aa8063a841b1172005397dec585214100b8
Tobias <tobias@fixmycity.de>
2024-06-24 11:09:42 +0200

    Map: Reorder layer

--- src/components/page_radnetz/mapData/pageBestand.const.tsx
+++ src/components/page_radnetz/mapData/pageBestand.const.tsx
@@ -6,147 +6,6 @@ export const pageBestand: MapDataAndLegend = {
   // "Bestand" in table = "Radinfrastruktur" in map / former website ?
   bestand: {
     sources: {
-      bikelanes: {
-        pmTilesUrl: 'https://radverkehrsatlas.de/api/uploads/website-nudafa-bikelanes',
-        layers: [
-          {
-            id: 'needsClarification',
-            type: 'line',
-            paint: {
-              'line-width': ['interpolate', ['linear'], ['zoom'], 8, 1.5, 10, 1.5, 14, 2, 16, 3],
-              'line-color': '#a97bea',
-              'line-dasharray': [2.5, 0.5],
-            },
-            filter: ['match', ['get', 'category'], ['needsClarification'], true, false],
-          },
-          {
-            id: 'Gehweg Rad frei',
-            type: 'line',
-            paint: {
-              'line-width': ['interpolate', ['linear'], ['zoom'], 8, 1.5, 10, 1.5, 14, 2, 16, 3],
-              'line-dasharray': [2, 2],
-              'line-color': '#9fb9f9',
-              'line-offset': ['interpolate', ['linear'], ['zoom'], 12, 0, 15, -1],
-            },
-            filter: [
-              'match',
-              ['get', 'category'],
-              [
-                'footwayBicycleYes_isolated',
-                'pedestrianAreaBicycleYes',
-                'footwayBicycleYes_adjoining',
-                'footwayBicycleYes_adjoiningOrIsolated',
-              ],
-              true,
-              false,
-            ],
-          },
-          {
-            id: 'Fuehrung mit Kfz-explizit',
-            type: 'line',
-            paint: {
-              'line-width': ['interpolate', ['linear'], ['zoom'], 8, 1.5, 10, 1.5, 14, 2, 16, 3],
-              'line-dasharray': [3, 1],
-              'line-color': '#0098f0',
-              'line-offset': ['interpolate', ['linear'], ['zoom'], 12, 0, 15, -1],
-            },
-            filter: [
-              'match',
-              ['get', 'category'],
-              [
-                'sharedMotorVehicleLane',
-                'bicycleRoad_vehicleDestination',
-                'sharedBusLaneBusWithBike',
-                'explicitSharedLaneButNoSignage',
-              ],
-              true,
-              false,
-            ],
-          },
-          {
-            id: 'Fuehrung mit Fussverkehr',
-            type: 'line',
-            paint: {
-              'line-width': ['interpolate', ['linear'], ['zoom'], 8, 1.5, 10, 1.5, 14, 2, 16, 3],
-              'line-dasharray': [3, 1],
-              'line-color': '#174ed9',
-              'line-offset': ['interpolate', ['linear'], ['zoom'], 12, 0, 15, -1],
-            },
-            filter: [
-              'match',
-              ['get', 'category'],
-              [
-                'footAndCyclewayShared_isolated',
-                'footAndCyclewayShared_adjoining',
-                'footAndCyclewayShared_adjoiningOrIsolated',
-                'livingStreet',
-              ],
-              true,
-              false,
-            ],
-          },
-          {
-            id: 'Fuehrung eigenstaendig auf Fahrbahn',
-            type: 'line',
-            paint: {
-              'line-width': ['interpolate', ['linear'], ['zoom'], 8, 1.5, 10, 1.5, 14, 2, 16, 3],
-              'line-color': '#0098f0',
-              'line-offset': ['interpolate', ['linear'], ['zoom'], 12, 0, 15, -1],
-            },
-            filter: [
-              'match',
-              ['get', 'category'],
-              [
-                'cyclewayOnHighway_exclusive',
-                'cyclewayOnHighwayBetweenLanes',
-                'cyclewayLink',
-                'crossing',
-                'cyclewayOnHighway_advisory',
-                'cyclewayOnHighway_advisoryOrExclusive',
-              ],
-              true,
-              false,
-            ],
-          },
-          {
-            id: 'fuehrung baul. abgesetzt von Kfz',
-            type: 'line',
-            paint: {
-              'line-width': ['interpolate', ['linear'], ['zoom'], 8, 1.5, 10, 1.5, 14, 2, 16, 3],
-              'line-color': '#174ed9',
-              'line-offset': ['interpolate', ['linear'], ['zoom'], 12, 0, 15, -1],
-            },
-            filter: [
-              'match',
-              ['get', 'category'],
-              [
-                'footAndCyclewaySegregated_adjoining',
-                'footAndCyclewaySegregated_adjoiningOrIsolated',
-                'cycleway_isolated',
-                'cycleway_adjoining',
-                'bicycleRoad',
-                'footAndCyclewaySegregated_isolated',
-                'cycleway_adjoiningOrIsolated',
-              ],
-              true,
-              false,
-            ],
-          },
-          {
-            id: 'hitarea-bikelanes',
-            type: 'line',
-            paint: {
-              'line-width': ['interpolate', ['linear'], ['zoom'], 9, 1, 14.1, 10, 22, 12],
-              'line-opacity': 0,
-              'line-color': 'hsl(290, 100%, 54%)',
-            },
-            filter: ['has', 'category'],
-            layout: {
-              'line-cap': 'round',
-            },
-          },
-        ],
-      },
       roads: {
         pmTilesUrl: 'https://radverkehrsatlas.de/api/uploads/website-nudafa-roads',
         layers: [
@@ -285,35 +144,6 @@ export const pageBestand: MapDataAndLegend = {
             },
           },
           {
-            layout: {
-              'line-cap': 'round',
-            },
-            filter: [
-              'match',
-              ['get', 'road'],
-              [
-                'residential',
-                'living_street',
-                'bicycle_road',
-                'pedestrian',
-                'unclassified',
-                'residential_priority_road',
-                'unspecified_road',
-                'service_road',
-                'service_alley',
-              ],
-              true,
-              false,
-            ],
-            type: 'line',
-            id: 'hitarea-roadclassification-sidepath copy 1',
-            paint: {
-              'line-width': ['interpolate', ['linear'], ['zoom'], 9, 1, 14.1, 10, 22, 12],
-              'line-opacity': 0,
-              'line-color': 'rgb(216, 20, 255)',
-            },
-          },
-          {
             minzoom: 13,
             layout: {
               'line-cap': 'square',
@@ -382,6 +212,134 @@ export const pageBestand: MapDataAndLegend = {
               'line-width': ['interpolate', ['linear'], ['zoom'], 13, 3, 15, 5, 22, 10],
               'line-opacity': 0.7,
             },
+          },
+        ],
+      },
+      bikelanes: {
+        pmTilesUrl: 'https://radverkehrsatlas.de/api/uploads/website-nudafa-bikelanes',
+        layers: [
+          {
+            id: 'needsClarification',
+            type: 'line',
+            paint: {
+              'line-width': ['interpolate', ['linear'], ['zoom'], 8, 1.5, 10, 1.5, 14, 2, 16, 3],
+              'line-color': '#a97bea',
+              'line-dasharray': [2.5, 0.5],
+            },
+            filter: ['match', ['get', 'category'], ['needsClarification'], true, false],
+          },
+          {
+            id: 'Gehweg Rad frei',
+            type: 'line',
+            paint: {
+              'line-width': ['interpolate', ['linear'], ['zoom'], 8, 1.5, 10, 1.5, 14, 2, 16, 3],
+              'line-dasharray': [2, 2],
+              'line-color': '#9fb9f9',
+              'line-offset': ['interpolate', ['linear'], ['zoom'], 12, 0, 15, -1],
+            },
+            filter: [
+              'match',
+              ['get', 'category'],
+              [
+                'footwayBicycleYes_isolated',
+                'pedestrianAreaBicycleYes',
+                'footwayBicycleYes_adjoining',
+                'footwayBicycleYes_adjoiningOrIsolated',
+              ],
+              true,
+              false,
+            ],
+          },
+          {
+            id: 'Fuehrung mit Kfz-explizit',
+            type: 'line',
+            paint: {
+              'line-width': ['interpolate', ['linear'], ['zoom'], 8, 1.5, 10, 1.5, 14, 2, 16, 3],
+              'line-dasharray': [3, 1],
+              'line-color': '#0098f0',
+              'line-offset': ['interpolate', ['linear'], ['zoom'], 12, 0, 15, -1],
+            },
+            filter: [
+              'match',
+              ['get', 'category'],
+              [
+                'sharedMotorVehicleLane',
+                'bicycleRoad_vehicleDestination',
+                'sharedBusLaneBusWithBike',
+                'explicitSharedLaneButNoSignage',
+                'livingStreet',
+              ],
+              true,
+              false,
+            ],
+          },
+          {
+            id: 'Fuehrung mit Fussverkehr',
+            type: 'line',
+            paint: {
+              'line-width': ['interpolate', ['linear'], ['zoom'], 8, 1.5, 10, 1.5, 14, 2, 16, 3],
+              'line-dasharray': [3, 1],
+              'line-color': '#174ed9',
+              'line-offset': ['interpolate', ['linear'], ['zoom'], 12, 0, 15, -1],
+            },
+            filter: [
+              'match',
+              ['get', 'category'],
+              [
+                'footAndCyclewayShared_isolated',
+                'footAndCyclewayShared_adjoining',
+                'footAndCyclewayShared_adjoiningOrIsolated',
+              ],
+              true,
+              false,
+            ],
+          },
+          {
+            id: 'Fuehrung eigenstaendig auf Fahrbahn',
+            type: 'line',
+            paint: {
+              'line-width': ['interpolate', ['linear'], ['zoom'], 8, 1.5, 10, 1.5, 14, 2, 16, 3],
+              'line-color': '#0098f0',
+              'line-offset': ['interpolate', ['linear'], ['zoom'], 12, 0, 15, -1],
+            },
+            filter: [
+              'match',
+              ['get', 'category'],
+              [
+                'cyclewayOnHighway_exclusive',
+                'cyclewayOnHighwayBetweenLanes',
+                'cyclewayLink',
+                'crossing',
+                'cyclewayOnHighway_advisory',
+                'cyclewayOnHighway_advisoryOrExclusive',
+              ],
+              true,
+              false,
+            ],
+          },
+          {
+            id: 'fuehrung baul. abgesetzt von Kfz',
+            type: 'line',
+            paint: {
+              'line-width': ['interpolate', ['linear'], ['zoom'], 8, 1.5, 10, 1.5, 14, 2, 16, 3],
+              'line-color': '#174ed9',
+              'line-offset': ['interpolate', ['linear'], ['zoom'], 12, 0, 15, -1],
+            },
+            filter: [
+              'match',
+              ['get', 'category'],
+              [
+                'footAndCyclewaySegregated_adjoining',
+                'footAndCyclewaySegregated_adjoiningOrIsolated',
+                'cycleway_isolated',
+                'cycleway_adjoining',
+                'bicycleRoad',
+                'footAndCyclewaySegregated_isolated',
+                'cycleway_adjoiningOrIsolated',
+              ],
+              true,
+              false,
+            ],
           },
         ],
       },
