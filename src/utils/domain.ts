144fa42771e403b4f5e66d62197c1f59bff04f8f
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2024-07-24 11:11:56 +0200

    update domain.ts

--- src/utils/domain.ts
+++ src/utils/domain.ts
@@ -3,15 +3,11 @@
 // CONTEXT https://docs.netlify.com/configure-builds/environment-variables/#build-metadata
 // URL vs. DEPLOY_PRIME_URL https://docs.netlify.com/configure-builds/environment-variables/#deploy-urls-and-metadata
 // NETLIFY https://docs.netlify.com/configure-builds/environment-variables/#build-metadata
-// todo - these are netlify specific
-// from where do we get URL and DEPLOY_PRIME_URL?
+// todo - in case we have a staging domain on IONOS, we should check if the build happens on Deploy Now or Netlify (process.env.NETLIFY) and differentiate between production and staging domain for SSR also on Deploy Now (or maybe not important as it is only used for social sharing so far)
 
 export const domain = () => {
   const ssrDomain =
     process.env.CONTEXT === 'production' ? process.env.URL : process.env.DEPLOY_PRIME_URL
-  // process.env.NODE_ENV ===  'production'
-  //   ? process.env.URL
-  //   : process.env.DEPLOY_PRIME_URL
 
   const ssrFallback = 'https://www.nudafa.de'
 
