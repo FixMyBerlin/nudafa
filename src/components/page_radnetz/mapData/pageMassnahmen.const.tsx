14a03cf6575b6e0f651d1ac36da068c96115e8bb
Tobias <tobias@fixmycity.de>
2024-07-25 12:00:02 +0200

    Update styles Massnahmen

--- src/components/page_radnetz/mapData/pageMassnahmen.const.tsx
+++ src/components/page_radnetz/mapData/pageMassnahmen.const.tsx
@@ -6,7 +6,90 @@ export const pageMassnahmen: MapDataAndLegend = {
     sources: {
       massnahmen: {
         pmTilesUrl: 'https://radverkehrsatlas.de/api/uploads/nudafa-massnahmen',
-        layers: [],
+        layers: [
+          {
+            id: 'massnahmen alle',
+            layout: {
+              'line-cap': 'round',
+            },
+            type: 'line',
+            paint: {
+              'line-color': [
+                'match',
+                ['get', 'Metadaten_V2_Zuständige Kommune'],
+                ['W'],
+                '#4abbf7',
+                ['Z'],
+                '#25a78d',
+                ['E'],
+                '#72ba5a',
+                [' S '],
+                '#eb3398',
+                '#5b11ee',
+              ],
+              'line-width': ['interpolate', ['linear'], ['zoom'], 8, 1, 12, 5],
+              'line-opacity': 0.77,
+            },
+          },
+        ],
+      },
+      netzentwurf: {
+        pmTilesUrl: 'https://radverkehrsatlas.de/api/uploads/nudafa-netzentwurf',
+        layers: [
+          {
+            id: 'nudafa-netzentwurf s_w',
+            type: 'line',
+            paint: {
+              'line-opacity': 0.7,
+              'line-color': [
+                'match',
+                ['get', 'radverkehrsatlas'],
+                ['2_hauptroute'],
+                '#000000',
+                ['3_radschnellverbindung'],
+                '#000000',
+                '#7d7d7d',
+              ],
+              'line-width': [
+                'interpolate',
+                ['linear'],
+                ['zoom'],
+                9,
+                [
+                  'match',
+                  ['get', 'radverkehrsatlas'],
+                  ['3_radschnellverbindung'],
+                  2,
+                  ['2_hauptroute'],
+                  1.5,
+                  1,
+                ],
+                12,
+                [
+                  'match',
+                  ['get', 'radverkehrsatlas'],
+                  ['3_radschnellverbindung'],
+                  4,
+                  ['2_hauptroute'],
+                  3,
+                  2,
+                ],
+              ],
+              'line-dasharray': [2, 1],
+            },
+            filter: [
+              'all',
+              ['match', ['geometry-type'], ['LineString'], true, false],
+              [
+                'match',
+                ['get', 'radverkehrsatlas'],
+                ['2_hauptroute', '1_nebenroute', '3_radschnellverbindung'],
+                true,
+                false,
+              ],
+            ],
+          },
+        ],
       },
     },
     legends: <LegendMassnahmen />,
