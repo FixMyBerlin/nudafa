2cd1c05a1abafeb118b6f31af1fc04886d2fd2fe
Tobias <tobias@fixmycity.de>
2024-07-25 12:34:44 +0200

    Update styles Radnetz

--- src/components/page_radnetz/mapData/pageLuftliniennetz.const.tsx
+++ src/components/page_radnetz/mapData/pageLuftliniennetz.const.tsx
@@ -4,18 +4,33 @@ import type { MapDataAndLegend } from './mapDataAndLegend.const'
 export const pageLuftliniennetz: MapDataAndLegend = {
   luftliniennetz: {
     sources: {
-      combined: {
-        pmTilesUrl: 'https://radverkehrsatlas.de/api/uploads/nudafa-combined',
+      netzentwurf: {
+        pmTilesUrl: 'https://radverkehrsatlas.de/api/uploads/nudafa-netzentwurf',
         layers: [
           {
-            id: 'Luftlinien',
-            filter: ['match', ['get', 'Typ'], ['', 'Wundschlinie'], true, false],
+            id: 'nudafa-luftlininien',
             type: 'line',
             paint: {
-              'line-color': '#3f74de',
-              'line-width': 3,
-              'line-opacity': ['interpolate', ['linear'], ['zoom'], 11.591, 0.7, 13, 0.67],
+              'line-color': '#3f52de',
+              'line-opacity': 0.8,
+              'line-width': [
+                'interpolate',
+                ['linear'],
+                ['zoom'],
+                8,
+                ['match', ['get', 'radverkehrsatlas'], ['c_luftlinie'], 2, 1],
+                12,
+                ['match', ['get', 'radverkehrsatlas'], ['c_luftlinie'], 5, 2],
+              ],
+              'line-dasharray': [2, 1],
             },
+            filter: [
+              'match',
+              ['get', 'radverkehrsatlas'],
+              ['c_luftlinie', 'd_nahverbindung'],
+              true,
+              false,
+            ],
           },
         ],
       },
@@ -23,26 +38,40 @@ export const pageLuftliniennetz: MapDataAndLegend = {
         pmTilesUrl: 'https://radverkehrsatlas.de/api/uploads/nudafa-ziel-zwangspunkte',
         layers: [
           {
-            filter: ['match', ['get', 'Siedlung'], [1], true, false],
+            id: 'nudafa-ramboll-zwangspunkte',
             type: 'circle',
-            id: 'zielpunkte',
             paint: {
-              'circle-opacity': 0,
-              'circle-stroke-opacity': ['interpolate', ['linear'], ['zoom'], 11.7, 1, 13.1, 1],
-              'circle-color': '#105ef9',
-              'circle-stroke-width': 3,
-              'circle-stroke-color': '#3f74de',
-              'circle-radius': 8,
-            },
-          },
-          {
-            filter: ['match', ['get', 'Zwangspunk'], [1], true, false],
-            type: 'circle',
-            id: 'zwangspunkte',
-            paint: {
-              'circle-opacity': 0.8,
+              'circle-stroke-width': 2,
               'circle-stroke-opacity': 0.8,
               'circle-color': '#3f74de',
+              'circle-radius': 4,
+              'circle-stroke-color': '#3f74de',
+            },
+            filter: ['match', ['get', 'Zwangspunk'], [1], true, false],
+          },
+          {
+            id: 'nudafa-zielpunkte',
+            type: 'line',
+            paint: {
+              'line-color': [
+                'match',
+                ['get', 'radverkehrsatlas'],
+                ['a_zentrum'],
+                '#235dd1',
+                '#103275',
+              ],
+              'line-width': ['match', ['get', 'radverkehrsatlas'], ['a_zentrum'], 5, 4],
+              'line-opacity': 0.73,
+            },
+            filter: [
+              'match',
+              ['get', 'radverkehrsatlas'],
+              ['a_zentrum', 'b_quartier'],
+              true,
+              false,
+            ],
+            layout: {
+              visibility: 'none',
             },
           },
         ],
