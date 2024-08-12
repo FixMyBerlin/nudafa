6107152c4e1078b3a5a92df9bf22007889466ad0
Henri <henri@fixmycity.de>
2024-08-12 16:17:34 +0200

    Use dynamic config based on OUTPUTMODE

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,25 +7,41 @@ import keystatic from '@keystatic/astro'
 import { defineConfig } from 'astro/config'
 
 import sitemap from '@astrojs/sitemap'
+import { loadEnv } from "vite";
 
-// https://astro.build/config
-export default defineConfig({
+// Load environment variables from .env
+const env = loadEnv(process.env.NODE_ENV, process.cwd(), "");
+
+const integrations = [
+  react(),
+  markdoc(),
+  mdx(),
+  tailwind(),
+  sitemap({
+    filter: (page) => page !== 'https://www.nudafa.de/radnetz/admin/',
+  }),
+]
+
+// If on environment is hybrid, add keystatic (for netlify), otherwise it's Deploy Now
+if (env.OUTPUTMODE === 'hybrid') {
+  integrations.push(keystatic());
+}
+
+const config = {
   site: 'https://www.nudafa.de',
-  integrations: [
-    react(),
-    markdoc(),
-    keystatic(),
-    mdx(),
-    tailwind(),
-    sitemap({
-      filter: (page) => page !== 'https://www.nudafa.de/radnetz/admin/',
-    }),
-  ],
-  output: 'hybrid',
+  integrations,
+  output: env.OUTPUTMODE,
   adapter: netlify(),
   redirects: {
     '/info': '/',
     '/forschungsprojekt': '/',
     '/radnetzplanung': '/radnetz/einleitung',
   },
-})
+};
+
+if (env.OUTPUTMODE === 'hybrid') {
+  config.adapter = netlify();
+}
+
+// https://astro.build/config
+export default defineConfig(config)
