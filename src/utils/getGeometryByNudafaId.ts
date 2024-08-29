b58cddd7211bc6d89f8792a82db8a5aa738e5e32
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2024-08-29 17:37:58 +0200

    Update getGeometryByNudafaId.ts

--- src/utils/getGeometryByNudafaId.ts
+++ src/utils/getGeometryByNudafaId.ts
@@ -1,10 +1,14 @@
 import measures from './../../data/measures/geometry.json'
 
-// todo return multiple features if there are multiple
-
 export const getGeometryByNudafaId = (nudafaId: string) => {
   const features = measures.features.filter(
     (feature) => feature.properties['NUDAFA_ID'] === nudafaId,
+  )
+  return features
+}
+export const getGeometryByNudafaIds = (nudafaIds: string[]) => {
+  const features = measures.features.filter((feature) =>
+    nudafaIds.includes(feature.properties['NUDAFA_ID']),
   )
   return features
 }
