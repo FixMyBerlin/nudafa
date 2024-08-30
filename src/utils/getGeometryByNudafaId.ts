790bd1e0f754890863bc8f1c8db83750ffaa863d
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2024-08-30 13:08:03 +0200

    fix getGeometryByNudafaIds

--- src/utils/getGeometryByNudafaId.ts
+++ src/utils/getGeometryByNudafaId.ts
@@ -10,7 +10,7 @@ export const getGeometryByNudafaId = (nudafaId: string): Feature[] => {
 }
 export const getGeometryByNudafaIds = (nudafaIds: string[]): Feature[] => {
   const features = measures.features
-    .filter((feature) => !feature.properties['NUDAFA_ID'])
+    .filter((feature) => feature.properties['NUDAFA_ID'])
     .filter((feature) => nudafaIds.includes(feature.properties['NUDAFA_ID']!))
   // @ts-ignore todo make sure we can be sure of geometry type - as we fetch the geo data locally, we have control over this
   return features as Feature[]
