59ca77bd5f42b24b0eff039c79261ef728981826
Henri <henri@fixmycity.de>
2024-09-24 15:39:46 +0200

    Retrieve Astro output mode from GitHub variable

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,17 @@ import { defineConfig } from 'astro/config'
 
 import sitemap from '@astrojs/sitemap'
 
+// USAGE:
+// `npm run dev` uses hybrid mode and keystatic()
+// `npm run build` (server) is based on .env and has different settings for Netlify (CMS/Keystatic) vs. Github Pages (Static site)
+// `npm run build:local && npm run serve` overwrites the .env settings to have a local test case for what is on Github Pages
+import { loadEnv } from 'vite'
+const { ASTRO_OUTPUT_MODE, ASTRO_USE_NETLIFY_ADAPTER } = loadEnv(
+  process.env.NODE_ENV,
+  process.cwd(),
+  '',
+)
+
 const integrations = [
   react(),
   markdoc(),
@@ -22,8 +33,11 @@ const integrations = [
 const config = {
   site: 'https://www.nudafa.de',
   integrations,
-  output: 'hybrid',
-  adapter: netlify(),
+  // On Netlify and during development we use `hybrid`, on Deploy Now we use `static`.
+  // Using static makes sure features like Astros redirecting work as expected.
+  // Docs https://docs.astro.build/en/basics/rendering-modes/
+  output: ASTRO_OUTPUT_MODE,
+  adapter: ASTRO_USE_NETLIFY_ADAPTER === 'true' ? netlify() : undefined,
   redirects: {
     '/info': '/',
     '/forschungsprojekt': '/',
