1ab5d47adfa84c5041366bacd5f62636208c6d77
Tobias <tobias@fixmycity.de>
2024-11-18 09:14:27 +0100

    Update tailwind glob pattern to include /keystatic

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,10 @@ import defaultTheme from 'tailwindcss/defaultTheme'
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: [
+    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+    './keystatic/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+  ],
   theme: {
     extend: {
       fontFamily: {
