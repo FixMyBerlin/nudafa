9d21faa672c118259590b5228dee9c417040e44a
Tobias <tobias@fixmycity.de>
2024-11-19 06:31:24 +0100

    Build: Copy build setup from other projects

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,44 @@
-import markdoc from '@astrojs/markdoc'
 import mdx from '@astrojs/mdx'
 import netlify from '@astrojs/netlify'
 import react from '@astrojs/react'
+import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import keystatic from '@keystatic/astro'
 import { defineConfig } from 'astro/config'
 
-import sitemap from '@astrojs/sitemap'
+// ABOUT:
+// We have to fetch settings from `.env`
+// Which we have to do manually, see https://docs.astro.build/en/guides/configuring-astro/#environment-variables
+//
+// USAGE:
+// `npm run dev` uses hybrid mode and keystatic()
+// `npm run build` (server) is based on .env and has different settings for Netlify (CMS/Keystatic) vs. IONOS (Static site)
+// `npm run build:local && npm run serve` overwrites the .env settings to have a local test case for what is on IONOS
+import { loadEnv } from 'vite'
+const { ASTRO_OUTPUT_MODE, ASTRO_USE_NETLIFY_ADAPTER } = loadEnv(
+  process.env.NODE_ENV,
+  process.cwd(),
+  '',
+)
 
-const integrations = [
-  react(),
-  markdoc(),
-  mdx(),
-  keystatic(),
-  tailwind(),
-  sitemap({
-    filter: (page) => page !== 'https://www.nudafa.de/radnetz/admin/',
-  }),
-]
-
-const config = {
+// CONFIG:
+// https://astro.build/config
+export default defineConfig({
   site: 'https://www.nudafa.de',
-  integrations,
-  output: 'hybrid',
-  adapter: netlify(),
+  integrations: [
+    ASTRO_OUTPUT_MODE === 'hybrid' ? keystatic() : undefined,
+    tailwind(),
+    react(),
+    mdx(),
+    sitemap({
+      filter: (page) => page !== 'https://www.nudafa.de/radnetz/admin/',
+    }),
+  ],
+  output: ASTRO_OUTPUT_MODE,
+  adapter: ASTRO_USE_NETLIFY_ADAPTER === 'true' ? netlify() : undefined,
   redirects: {
     '/info': '/',
     '/forschungsprojekt': '/',
     '/radnetzplanung': '/radnetz/einleitung',
   },
-}
-
-// https://astro.build/config
-export default defineConfig(config)
+})
