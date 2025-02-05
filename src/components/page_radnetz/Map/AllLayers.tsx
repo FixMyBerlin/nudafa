d20fec815b6d256a331f05fca560071d1c28b1a3
Tobias <tobias@fixmycity.de>
2025-02-05 15:15:54 +0100

    Map: Refactor Layer into a flat list

--- src/components/page_radnetz/Map/AllLayers.tsx
+++ src/components/page_radnetz/Map/AllLayers.tsx
@@ -1,5 +1,4 @@
 import { useStore } from '@nanostores/react'
-import React from 'react'
 import { Layer } from 'react-map-gl/maplibre'
 import { mapDataAndLegend } from '../mapData/mapDataAndLegend.const'
 import { mapDataBase } from '../mapData/mapDataBase.const'
@@ -14,51 +13,57 @@ export const AllLayers = ({ article }: Props) => {
   const articleSlug = article || router?.params.section
   if (!articleSlug) return null
 
-  const articleLayers = Object.entries(mapDataAndLegend).map(([slug, pageData]) => {
-    // We render all layer, but show only those that belong to the current article
-    // This way we can manage one beforIds list for all layers
-    const layersVisible = slug === articleSlug
-    return { sources: pageData.sources, visible: layersVisible, slug }
-  })
+  const articleLayers = Object.entries(mapDataAndLegend)
+    .map(([pageSlug, pageData]) => {
+      // We render all layer, but show only those that belong to the current article
+      // This way we can manage one beforIds list for all layers
+      const layersVisible = pageSlug === articleSlug
+      return Object.entries(pageData.sources).map(([sourceId, sources]) => {
+        return sources.layers.map((layer) => {
+          return { pageSlug, sourceId, pmTilesUrl: sources.pmTilesUrl, layersVisible, layer }
+        })
+      })
+    })
+    .flat(2)
 
-  const baseLayers = Object.entries(mapDataBase).map(([slug, sourceData]) => {
-    return { sources: { [slug]: sourceData }, visible: true, slug }
-  })
+  const baseLayers = Object.entries(mapDataBase)
+    .map(([sourceId, sourceData]) => {
+      return sourceData.layers.map((layer) => {
+        return {
+          pageSlug: 'base',
+          sourceId,
+          pmTilesUrl: sourceData.pmTilesUrl,
+          layersVisible: true,
+          layer,
+        }
+      })
+    })
+    .flat()
 
   const allLayers = [...articleLayers, ...baseLayers]
 
   return (
     <>
-      {allLayers.map(({ sources, visible, slug }) => {
-        return Object.entries(sources).map(([sourceId, sourceData]) => {
-          const sourceKey = `${slug}-${sourceId}`
+      {allLayers.map(({ pageSlug, sourceId, pmTilesUrl, layersVisible, layer }) => {
+        // Layers might have a layout property which we merge here with our visibility property
+        const visibility = layersVisible
+          ? ({ visibility: 'visible' } as const)
+          : ({ visibility: 'none' } as const)
+        const layerLayout = { ...visibility, ...(layer.layout || {}) }
 
-          return (
-            <React.Fragment key={sourceKey}>
-              {sourceData.layers?.map((layer) => {
-                // Layers might have a layout property which we merge here with our visibility property
-                const visibility = visible
-                  ? ({ visibility: 'visible' } as const)
-                  : ({ visibility: 'none' } as const)
-                const layerLayout = { ...visibility, ...(layer.layout || {}) }
+        const layerKey = `${pageSlug}-${sourceId}-${layer.id}`
 
-                const layerKey = `${slug}-${sourceId}-${layer.id}`
-
-                return (
-                  <Layer
-                    {...layer}
-                    source={sourceData.pmTilesUrl}
-                    key={layerKey}
-                    id={layerKey}
-                    source-layer="default"
-                    layout={layerLayout}
-                    beforeId="nudafa-beforeId-pagelayer"
-                  />
-                )
-              })}
-            </React.Fragment>
-          )
-        })
+        return (
+          <Layer
+            {...layer}
+            source={pmTilesUrl}
+            key={layerKey}
+            id={layerKey}
+            source-layer="default"
+            layout={layerLayout}
+            beforeId="nudafa-beforeId-pagelayer"
+          />
+        )
       })}
     </>
   )
